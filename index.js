c6178c354b210e68565044b4d20bcb316e80247e
JorgeRenan <jorgerenan94@gmail.com>
2026-02-04 22:13:07 -0300

    refatora rotas de produtos para uso de banco de dados e remove simulação com array

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
+require("dotenv").config();
 const express = require("express");// Importa o framework Express
 const database = require("./src/config/database");
-require("dotenv").config();
 
 const app = express();// Cria uma instância do aplicativo Express
 const port = 4505;// Define a porta onde o servidor irá rodar
 
-const db = []// Simula um banco de dados com um array
-
 app.use(express.json()) // Middleware para interpretar JSON no body das requisições
 
-app.get("/", (req, res) => {// Rota raiz
-    res.send("Olá Jorge Renan!")// Envia uma resposta simples
+
+app.get("/products", async (req, res) => {// Rota para obter todos os produtos
+    const result = await database.query(`SELECT * FROM products ORDER BY id DESC;`)// Consulta para selecionar todos os produtos ordenados por ID decrescente
+    res.send(result.rows)// Envia os dados dos produtos como resposta
 })
 
-app.post("/products", (req, res) => {// Rota para criar produtos
-    const name = req.body.name;// Extrai o nome do produto do corpo da requisição
-    const { price, category } = req.body;// Extrai o preço e a categoria do corpo da requisição
-    
-    if(!name || !price || !category){
-        return res.status(400).send("Preencha todos os campos!")
-    }
-
-    db.push({// Adiciona o novo produto ao "banco de dados" (array)
-        name,
-        category,
-        price
-    })
-
-    res.status(201).send("Produto criado com sucesso!")// Envia uma resposta de sucesso
+app.post("/products", async (req, res) => {// Rota para criar um novo produto
+    const { name, price, category_id } = req.body// Extrai os dados do corpo da requisição
+    const result = await database.query(`INSERT INTO products (name, price, category_id) VALUES ($1, $2, $3) RETURNING *;`, [name, price, category_id])// Consulta para inserir um novo produto e retornar o produto criado
+    res.send(result.rows[0])// Envia o produto criado como resposta
 })
 
-app.get("/products", (req, res) => {
-    res.send(db)// Envia a lista de produtos armazenados no "banco de dados"
+app.delete("/products/:id", async (req, res) => {
+    const { id } = req.params
+    const result = await database.query(`DELETE FROM products WHERE id = $1 RETURNING*;`, [id])
+    res.send(result.rows[0])
+})
+
+app.put("/products/:id", async (req, res) => {
+    const { id } = req.params
+    const { name, price, category_id} = req.body
+    const result = await database.query(`UPDATE products SET name = $1, price = $2, category_id = $3 WHERE id = $4 RETURNING *;`, [name, price, category_id, id])
+    res.send(result.rows[0])
+})
+
+app.patch("/products/price/:id", async (req, res) => {
+    const { id } = req.params
+    const { price } = req.body
+    const result = await database.query(`UPDATE products SET price = $1 WHERE id = $2 RETURNING *;`, [price, id])
+    res.send(result.rows[0])
+})
+
+app.get("/products/:id", async (req, res) => {
+    const { id } = req.params
+    const result = await database.query(`SELECT * FROM products WHERE id = $1;`, [id])
+    res.send(result.rows)
+})
+
+app.get("/products/name/:name", async (req,res) =>{
+    const { name } = req.params
+    const result = await database.query(`SELECT * FROM products WHERE name = $1;`, [name])
+    res.send(result.rows)
+})
+
+app.get("/products/name//:name", async (req,res) =>{
+    const { name } = req.params
+    const result = await database.query(`SELECT * FROM products WHERE name ILIKE $1;` ,[`%${name}%`])
+    res.send(result.rows)
 })
 
 app.listen(port, () => {// Inicia o servidor na porta especificada
