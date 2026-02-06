c267cf1bd4e2f1a20615de5bd28335c6fa6f43b5
JorgeRenan <jorgerenan94@gmail.com>
2026-02-06 11:23:48 -0300

    remove rota para buscar produtos pelo nome

--- index.js
+++ index.js
@@ -10,19 +10,6 @@ app.use(express.json()) // Middleware para interpretar JSON no body das requisiÃ
 
 app.use("/", routesProducts)// Usa as rotas definidas no arquivo routes/products.js
 
-app.get("/products/name//:name", async (req,res) =>{
-    const { name } = req.params
-
-    try {
-        const result = await database.query(`SELECT * FROM products WHERE name ILIKE $1;` ,[`%${name}%`])
-        res.send(result.rows)
-    } catch (error) {
-        console.error('Erro ao buscar os produtos:', error)
-        res.status(500).send({ error: 'Erro ao buscar os produtos'})
-    }
-    
-})
-
 app.listen(port, () => {// Inicia o servidor na porta especificada
     console.log(`Servidor rodando na porta ${port}`)// Loga uma mensagem quando o servidor estiver rodando
 })
