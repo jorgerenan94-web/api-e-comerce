7fdf807c8cfbecc4d1109927643804c8906f46ec
JorgeRenan <jorgerenan94@gmail.com>
2026-02-19 19:33:58 -0300

    refatora modelo de produtos, adicionando campos e melhorando a estrutura do modelo

--- src/models/products.js
+++ src/models/products.js
@@ -1,2 +1,45 @@
 const sequelize = require("../../config/sequelize");
 const { DataTypes } = require("sequelize");
+
+const Products = sequelize.define("Products",{
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    name: {
+        type:DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    original_price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: true
+    },
+    category_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: "Categories",
+            key: "id"
+        }
+    },
+    is_new: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    specfications: {
+        type: DataTypes.JSON,
+        allowNull: true
+    }
+})
+
+module.exports = Products;
