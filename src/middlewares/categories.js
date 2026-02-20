f871d546425a32681b9f3448504540acfe5a7a60
JorgeRenan <jorgerenan94@gmail.com>
2026-02-19 21:09:33 -0300

    adiciona middleware para validação de inserção de categorias

---
+++ src/middlewares/categories.js
@@ -0,0 +1,27 @@
+const Categories = require("../models/categories")
+
+async function validateInsertCategory(req, res, next){
+    const { name } = req.body
+
+    if( !name ){
+        return res.status(400).send({ error: "O campo name é obrigatório." })
+    }
+
+    if( name.length > 255 ){
+        return res.status(400).send({ error: "O campo name deve conter no máximo 255 caracteres." })
+    }
+
+    try {
+        const category = await Categories.findOne({ where: { name }})
+        if(category){
+            return res.status(400).send({ error: "Categoria já existe!" })
+        }
+    } catch (error) {
+        return res.status(500).send({ error: "Erro ao validar categoria." })
+    }
+    next()
+}
+
+module.exports = {
+    validateInsertCategory
+}
