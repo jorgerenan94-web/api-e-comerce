f8c646f3cda1a00ec3bb549afe6015dbd18ad9e8
JorgeRenan <jorgerenan94@gmail.com>
2026-03-03 20:55:41 -0300

    Correção na rota

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const usersControllers = require("../controllers/users");
 const usersMiddlewares = require("../middlewares/users");
 
-router = post("/users", usersMiddlewares.validateCreateUser, usersControllers.createUser);
+router.post("/users", usersMiddlewares.validateCreateUser, usersControllers.createUser);
 
 module.exports = router;
