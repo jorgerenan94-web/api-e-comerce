d00dc0b310764e9646cf477ea7beaf54a1008666
JorgeRenan <jorgerenan94@gmail.com>
2026-03-03 20:54:37 -0300

    Criação da routa de Users e configuração no index

---
+++ src/routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const usersControllers = require("../controllers/users");
+const usersMiddlewares = require("../middlewares/users");
+
+router = post("/users", usersMiddlewares.validateCreateUser, usersControllers.createUser);
+
+module.exports = router;
