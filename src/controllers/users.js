5de0988effe97c2254cf6a4dce0cf074cd47e6f9
JorgeRenan <jorgerenan94@gmail.com>
2026-03-03 19:36:24 -0300

    Função de criação de Usuário

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,7 +1,19 @@
+const { Users } = require("../models");
+
 async function createUser(req, res) {
     try {
+        await Users.create(req.body);
         
+        return res.status(201).send({ 
+            message: "Usuário criado com sucesso!" 
+        })
     } catch (error) {
-        
+        return res.status(500).send({ 
+            error: error.message
+        })
     }
+}
+
+module.exports = {
+    createUser
 }
