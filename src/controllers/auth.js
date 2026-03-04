598eaada98ec1f127e47d3ccb10584bb129b7dd7
JorgeRenan <jorgerenan94@gmail.com>
2026-03-03 22:05:04 -0300

    Criação do controle de autenticação do login do usuário

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,23 @@
+const jwt = require("jsonwebtoken")
+
 async function login (req, res) {
     try {
-        
+        const user = req.user;
+
+        const token = jwt.sign(
+            { id: user.id },
+            "secreto",
+            { expiresIn: "30d"}
+        )
+
+        return res.send({
+            token: token,
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+        })
     } catch (error) {
         
     }
