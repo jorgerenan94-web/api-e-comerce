782f5362f38b62c7b82edf8bcd0773573eff339f
JorgeRenan <jorgerenan94@gmail.com>
2026-04-16 19:47:10 -0300

    Implementação do arquivo de configuração do redis no projeto

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,1 +1,15 @@
-const {createCliente} = require("redis");
+const {createClient} = require("redis");
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+})
+
+redisClient.on("error", (err) => {
+    console.error(`Erro ao conectar ao Redis: ${err}`)
+})
+
+redisClient.connect()
+    .then(() => console.log("Redis conectado com sucesso!"))
+    .catch(console.error)
+
+module.exports = redisClient
